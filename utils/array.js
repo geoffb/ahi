6a23e3686c839db058d4e3d16b04e964b535f3d1
Geoff Blair <geoff@lostdecadegames.com>
2015-11-03 18:41:06 -0800

    Add utils/array.last

--- utils/array.js
+++ utils/array.js
@@ -3,6 +3,14 @@ exports.contains = function (arr, item) {
 	return index >= 0;
 };
 
+exports.last = function (arr) {
+	if (arr.length > 0) {
+		return arr[arr.length - 1];
+	} else {
+		return null;
+	}
+};
+
 exports.remove = function (arr, item) {
 	var index = arr.indexOf(item);
 	if (index >= 0) {
