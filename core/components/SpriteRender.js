0c822c2e46c6f60a82dcc9390fc8ff58753a6a91
Geoff Blair <geoff@lostdecadegames.com>
2015-11-20 17:37:26 -0800

    Add anchor point to SpriteRender component

--- core/components/SpriteRender.js
+++ core/components/SpriteRender.js
@@ -5,6 +5,7 @@ var images = require("../images");
 var exports = module.exports = function SpriteRender (conf) {
 	var conf = object.merge(conf, exports.defaults);
 	this.origin = new Vector2(conf.origin.x, conf.origin.y);
+	this.anchor = new Vector2(conf.anchor.x, conf.anchor.y);
 	this.width = conf.width;
 	this.height = conf.height;
 	this.imagePath = conf.imagePath;
@@ -14,6 +15,10 @@ exports.defaults = {
 	origin: {
 		x: 0,
 		y: 0
+	},
+	anchor: {
+		x: 0.5,
+		y: 0.5
 	},
 	width: 1,
 	height: 1,
@@ -25,9 +30,13 @@ var proto = exports.prototype;
 proto.render = function SpriteRender_render (ctx) {
 	if (!this.imagePath) { return; }
 	var image = images.get(this.imagePath);
+
+	var destX = -Math.round(this.width * this.anchor.x);
+	var destY = -Math.round(this.height * this.anchor.y);
+
 	ctx.drawImage(
 		image,
 		this.origin.x, this.origin.y, this.width, this.height,
-		-this.width / 2, -this.height / 2, this.width, this.height
+		destX, destY, this.width, this.height
 	);
 };
